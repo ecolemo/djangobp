5937daba499d87dfe29efd76a77789698655bbd7
Youngrok pak <pak.youngrok@gmail.com>
2011-11-10 16:57:18 +0900

    refactoring

--- djangobp/route.py
+++ djangobp/route.py
@@ -1,5 +1,11 @@
-import sys
+from django.http import HttpResponse, HttpResponseServerError
+from django.utils import importlib
 from django.utils.functional import curry
+from mako import exceptions
+from mako.lookup import TemplateLookup
+from mako.template import Template
+import os
+import sys
 
 controller_resource_method_pattern = r'(?P<controller>[^/\?\&]+)(/(?P<resource_id>[^/\?\&]+))?(/(?P<method>[^/\?\&]+))?'
 
@@ -21,3 +27,18 @@ def route(controllers_root, request, controller='root', resource_id=None, method
         else: method = 'index'
         
     return getattr(sys.modules[module_name], method)(request, resource_id)
+
+
+def render_to_response(filename, dictionary):
+    # using app_name that is set by route()
+    app_name = dictionary['request'].app_name
+    print app_name
+    templates = importlib.import_module(app_name + '.templates')
+    try:
+        print templates.__path__[0]
+        lookup = TemplateLookup(directories=[templates.__path__[0]], input_encoding='utf8')
+        template = Template(filename=templates.__path__[0] + os.sep + filename, input_encoding='utf8', output_encoding='utf8', lookup=lookup)
+        return HttpResponse(template.render(**dictionary))
+    except:
+        return HttpResponseServerError(exceptions.html_error_template().render())
+        
