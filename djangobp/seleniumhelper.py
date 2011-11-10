7a4f0d9f3bb262b1ca79452831536ca63e48ec7a
Youngrok pak <pak.youngrok@gmail.com>
2011-11-10 14:38:40 +0900

    selenium 2.11

--- djangobp/seleniumhelper.py
+++ djangobp/seleniumhelper.py
@@ -1,17 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.common.exceptions import TimeoutException
-from selenium.webdriver.firefox.extensionconnection import ExtensionConnection
 import time
+from selenium.common.exceptions import TimeoutException
+import os
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+firefox_profiles_path = os.environ['HOME'] + os.sep + 'Library/Application Support/Firefox/Profiles'
 
 def webdriver_firefox():
+    
     try:
-#        driver = webdriver.Remote(command_executor=ExtensionConnection(),
-#                                       browser_name='firefox', platform='ANY', version='',
-#                                       javascript_enabled=True)
-        driver = webdriver.Remote(command_executor='http://localhost:7055/hub')
+        f = open(os.environ['HOME'] + os.sep + '.selenium_port', 'r')
+        port = f.read()
+        f.close()
+        driver = webdriver.Remote(command_executor='http://127.0.0.1:%s/hub' % port, desired_capabilities=DesiredCapabilities.FIREFOX)
     except:
-        profile = webdriver.FirefoxProfile('selenium')
+        default_profile = filter(lambda s: s.find('.selenium') >= 0, os.listdir(firefox_profiles_path))[0]
+        profile = webdriver.FirefoxProfile(firefox_profiles_path + os.sep + default_profile)
         driver = webdriver.Firefox(profile)
+        f = open(os.environ['HOME'] + os.sep + '.selenium_port', 'w')
+        f.write(str(profile.port))
+        f.close()
     return driver
 
 def wait_until(func, timeout=3):
